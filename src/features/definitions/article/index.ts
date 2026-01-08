b9a7ec8cd5caf2694894971a79d8c3bf9167c9d1
Invictus Navarchus <115978750+InvictusNavarchus@users.noreply.github.com>
2026-01-09 05:47:38 +0700

    refactor: sort import orders in article index
    Biome doesn't sort this because it doesn't know which import should be executed first. Since the import execution order actually doesn't matter here, we can sort it alphabetically.
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- src/features/definitions/article/index.ts
+++ src/features/definitions/article/index.ts
@@ -3,9 +3,9 @@
  * Import this file to register all article page features
  */
 
-import "./hideRightSidebar";
 import "./centerArticle";
-import "./readingStats";
-import "./tableOfContents";
 import "./copyArticle";
 import "./engagementMover";
+import "./hideRightSidebar";
+import "./readingStats";
+import "./tableOfContents";
